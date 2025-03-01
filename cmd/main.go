b258f991e5349a04c168374c7770310bf5b80a4d
xSaCh <samarthchauhan7@gmail.com>
2025-03-01 14:57:41 +0530

    Logging incomming request

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	xginx "github.com/xSaCh/xginx/pkg"
+)
+
+const PORT = 5000
 
 func main() {
-	fmt.Println("Hello, World!")
+	lb := xginx.NewLoadBalancer()
+	fmt.Printf("Running xginx on %d\n", PORT)
+	http.ListenAndServe(fmt.Sprintf(":%d", PORT), http.HandlerFunc(lb.Router))
 }
--- pkg/xginx.go
+++ pkg/xginx.go
@@ -1,1 +1,26 @@
 package xginx
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type LoadBalancer struct {
+}
+
+func NewLoadBalancer() *LoadBalancer {
+	return &LoadBalancer{}
+}
+
+func (lb *LoadBalancer) Router(w http.ResponseWriter, r *http.Request) {
+	lg := fmt.Sprintf("Received request from %s\n", r.RemoteAddr)
+	lg += fmt.Sprintf("%s %s %s\n", r.Method, r.RequestURI, r.Proto)
+	for key, values := range r.Header {
+		for _, value := range values {
+			lg += fmt.Sprintf("%s: %s\n", key, value)
+		}
+	}
+	log.Print(lg)
+	w.Write(fmt.Appendf(nil, "Hello from %s", r.RequestURI))
+}
